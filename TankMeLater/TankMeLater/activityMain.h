5280a6a8a2f68805f0835ed255441f68d01696f4
Owen Collins <ocollin1@bl-n36.pods.bu.int>
2019-02-16 14:03:53 -0500

    Built main Activity screen, positiond main objects

---
+++ TankMeLater/TankMeLater/activityMain.h
@@ -0,0 +1,21 @@
+//
+//  activityMain.h
+//  TankMeLater
+//
+//  Created by Owen Collins on 2/16/19.
+//  Copyright © 2019 Owen Collins. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface activityMain : ViewController{
+    
+    IBOutlet UIImage *tank;
+    IBOutlet UIButton *LeftArrow;
+    IBOutlet UIButton *RightArrow;
+    IBOutlet UIImageView *ImageView;
+    
+    
+}
+
+@end
