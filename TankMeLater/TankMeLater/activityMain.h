42451025c9a2b06a9fa03b0a3096612a8ed1e6dd
Owen Collins <ocollin1@slic-41.pods.bu.int>
2019-02-22 10:36:07 -0500

    Changed activityMain header and code to more closely follow pmaddens asteroid code as an example

--- TankMeLater/TankMeLater/activityMain.h
+++ TankMeLater/TankMeLater/activityMain.h
@@ -5,12 +5,12 @@
 //  Created by Owen Collins on 2/16/19.
 //  Copyright © 2019 Owen Collins. All rights reserved.
 //
-
+/*
 #import "ViewController.h"
 
 @interface activityMain : ViewController{
     
-    IBOutlet UIImage *tank;
+    IBOutlet UIImageView *tank;
     IBOutlet UIButton *LeftArrow;
     IBOutlet UIButton *RightArrow;
     IBOutlet UIImageView *ImageView;
@@ -19,3 +19,20 @@
 }
 
 @end
+*/
+
+#import <UIKit/UIKit.h>
+//#import "Boulder.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GameView : UIView
+@property (nonatomic, strong) UIImageView *tank;
+@property float dx, dy;
+@property (nonatomic, strong) NSTimer *timer;
+//@property BOOL rotating_l, rotating_r;
+
+//@property (nonatomic, strong) NSMutableArray *boulders;
+@end
+
+NS_ASSUME_NONNULL_END
--- TankMeLater/TankMeLater/activityMain.m
+++ TankMeLater/TankMeLater/activityMain.m
@@ -8,6 +8,11 @@
 
 #import "activityMain.h"
 
+@implementation GameView
+@synthesize tank;
+
+
+/*
 @interface activityMain ()
 
 @end
@@ -17,17 +22,36 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+}*/
 
+-(id)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super initWithCoder:aDecoder];
+    if (self)
+    {
+        tank = [[UIImageView alloc] initWithFrame:CGRectMake(100, 100, 150, 100)];
+        [tank setImage:[UIImage imageNamed:@"tank"]];
+        [self addSubview:tank];
+        
+        
+        self.timer = [NSTimer scheduledTimerWithTimeInterval:.1 repeats:YES block:^(NSTimer * _Nonnull timer) {
+            [self performSelectorOnMainThread:@selector(tick:) withObject:self.timer waitUntilDone:NO];
+        }];
+    }
     
-    
-    
+    return self;
 }
 
+
+
+/*
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+ */
 /*
 #pragma mark - Navigation
 
